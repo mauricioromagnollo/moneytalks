4dd9409befd5e5f4aced42de91f46e089ffb82f9
x0n4d0 <romagnolimauricio@gmail.com>
2021-10-10 12:15:46 -0300

    add npm global installations

---
+++ shared/npm-global-installations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npm install -g create-react-app 
+npm install -g create-next-app 
+npm install -g @nestjs/cli
+npm install -g yo generator-code
+npm install -g ts-node
+npm install -g expo-cli
+npm install -g react-native-cli 
+npm install -g nodemon
+npm install -g typescript
